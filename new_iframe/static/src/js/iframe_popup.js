3bcda6d373fd52a3b54d713cfe88f9e302e7821e
Joseph Hill <66689032+captivea-jhi@users.noreply.github.com>
2020-09-15 11:58:34 -0700

    Update iframe_popup.js

--- new_iframe/static/src/js/iframe_popup.js
+++ new_iframe/static/src/js/iframe_popup.js
@@ -10,35 +10,18 @@ const Widget = require('web.Widget');
 const IframePopup = Widget.extend({
     template: 'iframe_popup',
     events: {  },
-    /**
-     * @constructor
-     */
-    init() {
-      this._isClosed = true;
-    },
-    /**
-     * @override
-     */
+
     async start() {
 
-        this.$el.css('bottom', 0);
-        this.$el.css('right', 0);
         this.$el.hide();
 
         core.bus.on('toggle_iframe', this, this.toggleIframe);
 
-        this.call('bus_service', 'onNotification', this, this._onLongpollingNotifications);
+        //this.call('bus_service', 'onNotification', this, this._onLongpollingNotifications);
     },
 
     toggleIframe() {
-      if (this._isClosed) {
-        this.$el.show();
-        this._isClosed = false;
-      }
-      else {
-        this.$el.hide();
-        this._isClosed = true;
-      }
+      this.$el.toggle();
 
     },
 
