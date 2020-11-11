97864b9563918cd02962a801b1cc962fca359bcf
roko.kokeza@8x8.com <roko.kokeza@8x8.com>
2020-11-11 11:15:35 +0100

    All in one module

--- addons/8x8-integration/__manifest__.py
+++ addons/8x8-integration/__manifest__.py
@@ -9,29 +9,31 @@
         Odoo REST API
     """,
 
-    'author': "Yezileli Ilomo",
-    'website': "https://github.com/yezyilomo/odoo-rest-api",
+    'author': "Roko Kokeza",
+    'website': "https://8x8.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'developers',
-    'version': '0.1',
+    'version': '0.9',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/auth_page.xml',
+        'views/authorization.xml',
         'views/views.xml',
         'views/templates.xml',
+        'views/virtual_office_template.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'qweb': ['static/src/xml/*.xml'],
 
     "application": True,
     "installable": True,
